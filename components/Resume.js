70d11fb092b5d34894c43e347352d00caf833b80
Aman Shukla <amanks7400@gmail.com>
2022-08-26 14:42:31 +0530

    Add Resume Card

---
+++ components/Resume.js
@@ -0,0 +1,10 @@
+export default function Resume(){
+    return (
+        <div className="w-4/6 p-16 bg-[#D9D9D990] rounded-lg my-16 round-lg">
+                <h1>
+                    RESUME
+                </h1>
+        </div>
+
+    )
+};
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,21 @@
 import Head from 'next/head'
+import Resume from '../components/Resume'
 
 
 
 export default function Home() {
   return (
-    <div className="h-screen bg-slate-400" style={{
+    <div className="h-screen bg-slate-400 flex justify-center items-center" style={{
       backgroundImage: 'url("/bg.jpeg")',
       backgroundSize:"cover",
-      overdlowY:"scroll"
+      overflowY:"scroll"
     }} >
       <Head>
         <title>Aman Shukla | Full Stack Developer</title>
         <meta name="description" content="Digital Resume by Aman Shukla powered by Next Js and Tailwind" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1 className="text-3xl font-mono underline">
-        Hello world!
-      </h1>
+     <Resume />
     </div>
   )
 }
