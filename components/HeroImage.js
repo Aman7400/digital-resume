6b77924a7cbd401739ce1652c3b0190587eeb631
Aman Shukla <amanks7400@gmail.com>
2022-08-26 20:28:00 +0530

    Add profile pic

--- components/HeroImage.js
+++ components/HeroImage.js
@@ -2,10 +2,10 @@
 export default function HeroImage() {
     return (
         <section className="flex-1 flex justify-center items-center mb-8">
-            <img alt="dummy profile pic" src="/dummy.jpeg" style={{
+            <img alt="dummy profile pic" className="grayscale hover:grayscale-0 cursor-pointer  " src="/dummy.jpeg" style={{
                 width: 240,
                 height: 240,
-                borderRadius: "100%"
+                borderRadius: "100%",
             }} />
         </section>
     )
