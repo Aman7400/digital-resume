38400a164dd8241177c20aa431e0ba86660d3599
Aman Shukla <amanks7400@gmail.com>
2022-08-27 09:08:16 +0530

    Update Footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,17 +4,17 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="p-4 flex flex-col items-center justify-center text-white">
+        <div className="p-4 flex flex-col items-center text-subHeading justify-center text-white">
             <h1 className="font-mono  font-bold text-md">
                 Built with 
-               <Icon icon="bi:balloon-heart-fill" className=" ml-2 pulse inline-block" />
+               <Icon icon="bi:balloon-heart-fill" className=" ml-2 pulse inline-block text-heading" />
             </h1>
             <p className="my-1 font-mono italic font-light text-xs">
                 by
             </p>
-            <p className="font-bold font-mono text-xs cursor-pointer ">
+            <p className="font-bold font-mono text-xs text-heading cursor-pointer ">
                 <Link href="https://github.com/rockstar-technologies">
-                    <span className=' hover:text-[#8107c3] hover:shadow-md'>
+                    <span className=''>
                         Rockstar Technologies
                     </span>
                 </Link> &copy; {new Date().getFullYear()}
