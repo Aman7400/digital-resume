a0ad29713e137c2f7fdea1723c1d09b1ec455040
Aman Shukla <amanks7400@gmail.com>
2022-08-27 11:16:16 +0530

    Fix Profile Zoom In

--- components/HeroImage.js
+++ components/HeroImage.js
@@ -1,12 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
 export default function HeroImage() {
     return (
-        <section className="flex-1 flex justify-center items-center mb-8">
-            <img alt="dummy profile pic" src="/dummy.jpeg" style={{
-                width: 240,
-                height: 240,
-                borderRadius: "100%",
-            }} />
+        <section className=" flex justify-center h-64 shadow-lg w-64 cursor-pointer overflow-hidden mx-auto rounded-full items-center mb-8">
+            <img alt="dummy profile pic" className="hover:scale-105 transition-all ease-in-out " src="/dummy.jpeg"  />
         </section>
     )
 }
